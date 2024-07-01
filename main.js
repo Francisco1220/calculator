fbef7798e86f84c23ddc0470e0a1855b47e5b1b3
Francisco <franciscocolombocouste@gmail.com>
2024-07-01 17:04:13 +0200

    Added basic math functions and operate function that calls each

---
+++ main.js
@@ -0,0 +1,36 @@
+// 3 variables for each: 1 for the first number. 1 for the operator. 1 for the second number
+
+let add = function(a,b) {
+    return a + b;
+}
+
+let subtract = function(a, b) {
+    return a - b;
+}
+
+let multiply = function(a, b) {
+    return a * b;
+}
+
+let divide = function(a, b) {
+    return a / b;
+}
+
+// Takes an operator and 2 numbers and then calls one of the above functions on the numbers
+let operate = function(operator, first, second) {
+    if (operator == "+") {
+        let result = add(first, second);
+        return result;
+    } else if (operator == "-") {
+        let result = subtract(first, second);
+        return result;
+    } else if (operator == "*") {
+        let result = multiply(first, second);
+        return result;
+    } else {
+        let result = divide(first, second);
+        return result;
+    }
+}
+
+console.log(operate("-", 5, 3));
