4d90dc3bd3d9f1c8707b07289646abefd28a1b9f
Francisco <franciscocolombocouste@gmail.com>
2024-07-05 00:14:11 +0200

    Deleted most of javascript code because of several problems with its logic and clarity

--- main.js
+++ main.js
@@ -55,70 +55,9 @@ const changeColourBtn = function() {
 
 changeColourBtn ();
 
-const displayTxt = document.querySelector(".display")
+const displayTxt = document.querySelector(".display");
 
-// checks if equalSign is clicked, and increments accordingly
-let equalBtn = document.querySelector("#equal");
-let equalCounter = 0;
 
-/* updates text on display screen
-for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener("click", (e) => {
-        if (btn[i].innerHTML === "%" ||
-            btn[i].innerHTML === "/" ||
-            btn[i].innerHTML === "x" ||
-            btn[i].innerHTML === "-" ||
-            btn[i].innerHTML === "+") {
-            displayTxt.innerHTML += "";
-            // Note: parseInt is not returning a decimal number. Only an integer. Fix later
-            let firstVal = parseInt(displayTxt.innerHTML);
-            let operator = e.target.innerHTML;
-            console.log(operator);
-            console.log(firstVal);
-        } else {
-            displayTxt.innerHTML += e.target.innerHTML;
-        }
-    });
-} */
 
-let j = 0;
-let operator;
-let firstVal;
-let secondVal;
-for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener("click", (e) => {
-        if (btn[i].innerHTML !== "%" &&
-            btn[i].innerHTML !== "/" &&
-            btn[i].innerHTML !== "x" &&
-            btn[i].innerHTML !== "-" &&
-            btn[i].innerHTML !== "+" &&
-            btn[i].innerHTML !== "AC" &&
-            btn[i].innerHTML !== "+/-") {
-                displayTxt.innerHTML += e.target.innerHTML;
-        } else if (btn[i].innerHTML === "%" ||
-                   btn[i].innerHTML === "/" ||
-                   btn[i].innerHTML === "x" ||
-                   btn[i].innerHTML === "-" ||
-                   btn[i].innerHTML === "+" ||
-                   btn[i].innerHTML === "AC"||
-                   btn[i].innerHTML === "+/-") {
-            j++;
-              if (j === 1) {
-                // Note: parseInt is not returning a decimal number. Only an integer. Fix later
-                firstVal = parseInt(displayTxt.innerHTML);
-                displayTxt.innerHTML = "";
-                operator = e.target.innerHTML;
-                console.log(operator);
-                console.log(firstVal);
-              } else if (j === 2) {
-                secondVal = parseInt(displayTxt.innerHTML);
-                displayTxt.innerHTML = "";
-                console.log(secondVal);
-                j = 0;
-                result = operate(operator, firstVal, secondVal);
-                displayTxt.innerHTML = `${result}`;
-                firstVal = result;
-              }
-           }
-    });
-}
+
+
