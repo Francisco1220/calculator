dfa3c12480cd4f1b605dfbab591d85da5594e069
Francisco <franciscocolombocouste@gmail.com>
2024-07-05 14:28:31 +0200

    Add extra classes and ids

--- main.js
+++ main.js
@@ -33,11 +33,50 @@ let operate = function(operator, first, second) {
     }
 }
 
-// Adds classes to each button
+// Adds respective classes and ids for each button
 const btn = document.querySelectorAll("button");
 for(let i = 0; i < btn.length; i++) {
-    btn[i].setAttribute("class", "btn");
+    if (btn[i].innerHTML !== "+" ||
+        btn[i].innerHTML !== "-" ||
+        btn[i].innerHTML !== "/" ||
+        btn[i].innerHTML !== "x" ||
+        btn[i].innerHTML !== "%" ) {
+            btn[i].setAttribute("class", "btn");
+        }
+    if (btn[i].innerHTML === "+" ||
+        btn[i].innerHTML === "-" ||
+        btn[i].innerHTML === "/" ||
+        btn[i].innerHTML === "x" ||
+        btn[i].innerHTML === "%") {
+            btn[i].setAttribute("class", "btn operatorBtn");
+        }
+    if (btn[i].innerHTML === "=") {
+        btn[i].setAttribute("id", "equal");
+    }
+    if (btn[i].innerHTML === "AC") {
+        btn[i].setAttribute("id", "clearBtn");
+    }
+    if (btn[i].innerHTML === "+/-") {
+        btn[i].setAttribute("id", "makeNegBtn");
+    }
+    if (btn[i].innerHTML === ".") {
+        btn[i].setAttribute("id", "makeDecBtn");
+    } 
+    if (btn[i].innerHTML === "0" ||
+        btn[i].innerHTML === "1" ||
+        btn[i].innerHTML === "2" ||
+        btn[i].innerHTML === "3" ||
+        btn[i].innerHTML === "4" ||
+        btn[i].innerHTML === "5" ||
+        btn[i].innerHTML === "6" ||
+        btn[i].innerHTML === "7" ||
+        btn[i].innerHTML === "8" ||
+        btn[i].innerHTML === "9") {
+        btn[i].setAttribute("class", "btn numBtn");
+    }
 }
+
+console.log(btn);
 
 const changeColourBtn = function() {
     for(let i = 0; i < btn.length; i++) {
@@ -53,9 +92,8 @@ const changeColourBtn = function() {
     }
 }
 
-changeColourBtn ();
-
 const displayTxt = document.querySelector(".display");
+
 
 
 
